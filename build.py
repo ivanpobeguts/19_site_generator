b52939a0220ff8761c70955d998b40a62aac0c27
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-07-10 11:51:02 +0300

    move code from main to methods

--- build.py
+++ build.py
@@ -13,28 +13,37 @@ def load_markdown_article(path):
         return file.read()
 
 
+def save_page(path, content):
+    with open(path, 'w', encoding='utf8') as f:
+        f.write(content)
+
+
+def get_jinja_env():
+    return Environment(loader=FileSystemLoader(
+        searchpath='templates/'),
+        extensions=[MarkdownExtension]
+    )
+
+
 def get_articles_by_topic(articles_info, topic):
     return [article for article in articles_info['articles'] if article['topic'] == topic]
 
 
-if __name__ == '__main__':
-    articles_info = load_config()
-    # print(articles_info['topics'])
-
-    env = Environment(loader=FileSystemLoader(
-        searchpath='templates/'),
-        extensions=[MarkdownExtension]
-    )
+def render_pages(articles_info):
+    env = get_jinja_env()
     index_template = env.get_template('index.html')
     display_dictionary = {'topics': articles_info['topics']}
-    output = index_template.render(display_dictionary)
-    with open('static/index.html', 'w', encoding='utf8') as f:
-        f.write(output)
-
+    index_output = index_template.render(display_dictionary)
+    save_page('static/index.html', index_output)
     for article in articles_info['articles']:
         article_template = env.get_template('article.html')
         article_data = load_markdown_article('articles/{}'.format(article['source']))
         display_dictionary = {'article_data': article_data}
-        output = article_template.render(display_dictionary)
-        with open('static/{}.html'.format(article['source'].split('/')[1]), 'w', encoding='utf8') as f:
-            f.write(output)
+        article_output = article_template.render(display_dictionary)
+        acticle_html_path = 'static/{}.html'.format(article['source'].split('/')[1].split('.')[0])
+        save_page(acticle_html_path, article_output)
+
+
+if __name__ == '__main__':
+    articles_info = load_config()
+    render_pages(articles_info)
